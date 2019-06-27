15ce62991b0ea8a47e7d707d6429e7f799e06f81
Prasanna Joshi <prasanna_3003@yahoo.com>
2019-06-27 11:58:01 +1000

    go back to defdelegate & package tuple in game

--- test/hangman/game_test.exs
+++ test/hangman/game_test.exs
@@ -24,7 +24,7 @@ defmodule Hangman.GameTest do
     test "state doesn't change when the game has been won or lost already" do
       for state <- [:won, :lost] do
         game = Game.new_game() |> Map.put(:state, state)
-        assert ^game = Game.make_move(game, "x")
+        assert { ^game, _tally } = Game.make_move(game, "x")
       end
     end
   end
@@ -34,37 +34,37 @@ defmodule Hangman.GameTest do
       game = Game.new_game()
 
       # a capital ascii character
-      game = Game.make_move(game, "A")
+      { game, _tally } = Game.make_move(game, "A")
       assert game.state == :invalid_guess
       assert game.turns_left == 7
 
       # a non-ascii character
-      game = Game.make_move(game, "ó")
+      { game, _tally } = Game.make_move(game, "ó")
       assert game.state == :invalid_guess
       assert game.turns_left == 7
 
       # a string of more than one characters
-      game = Game.make_move(game, "a string")
+      {  game, _tally } = Game.make_move(game, "a string")
       assert game.state == :invalid_guess
       assert game.turns_left == 7
 
       # a charlist of one character
-      game = Game.make_move(game, 'a')
+      { game, _tally } = Game.make_move(game, 'a')
       assert game.state == :invalid_guess
       assert game.turns_left == 7
 
       # a charlist of more than one character
-      game = Game.make_move(game, 'abc')
+      { game, _tally } = Game.make_move(game, 'abc')
       assert game.state == :invalid_guess
       assert game.turns_left == 7
 
       # a number
-      game = Game.make_move(game, 321)
+      { game, _tally } = Game.make_move(game, 321)
       assert game.state == :invalid_guess
       assert game.turns_left == 7
 
       # a list of strings
-      game = Game.make_move(game, ["this", "is", "not", "acceptable"])
+      { game , _tally } = Game.make_move(game, ["this", "is", "not", "acceptable"])
       assert game.state == :invalid_guess
       assert game.turns_left == 7
     end
@@ -72,23 +72,23 @@ defmodule Hangman.GameTest do
     test "first time guess is accepted" do
       game = Game.new_game()
 
-      game = Game.make_move(game, "x")
+      { game , _tally } = Game.make_move(game, "x")
       assert game.state != :already_used
     end
 
     test "second time guess is not accepted" do
       game = Game.new_game()
 
-      game = Game.make_move(game, "x")
+      { game , _tally } = Game.make_move(game, "x")
       assert game.state != :already_used
-      game = Game.make_move(game, "x")
+      { game , _tally } = Game.make_move(game, "x")
       assert game.state == :already_used
     end
 
     test "sets game state to :good_guess when the guess is good but all letters have not been guessed yet" do
       game = Game.new_game("lorem")
 
-      game = Game.make_move(game, "o")
+      { game , _tally } = Game.make_move(game, "o")
       assert game.state == :good_guess
       assert game.turns_left == 7
     end
@@ -104,7 +104,7 @@ defmodule Hangman.GameTest do
         {"l", :won}
       ]
       |> Enum.reduce(game, fn {guess, state}, tracker_game ->
-        tracker_game = Game.make_move(tracker_game, guess)
+        { tracker_game , _tally } = Game.make_move(tracker_game, guess)
         assert tracker_game.state == state
         assert tracker_game.turns_left == 7
         tracker_game
@@ -114,7 +114,7 @@ defmodule Hangman.GameTest do
     test "sets game state to :bad_guess for a bad guess when there are still turns remaining" do
       game = Game.new_game("steeple")
 
-      game = Game.make_move(game, "x")
+      { game , _tally } = Game.make_move(game, "x")
       assert game.state == :bad_guess
     end
 
@@ -122,7 +122,7 @@ defmodule Hangman.GameTest do
       game = Game.new_game("steeple")
 
       assert game.turns_left == 7
-      game = Game.make_move(game, "x")
+      { game , _tally } = Game.make_move(game, "x")
       assert game.turns_left == 6
     end
 
@@ -139,7 +139,7 @@ defmodule Hangman.GameTest do
         {"z", :lost, 0}
       ]
       |> Enum.reduce(game, fn {guess, state, turns_left}, tracker_game ->
-        tracker_game = Game.make_move(tracker_game, guess)
+        { tracker_game , _tally } = Game.make_move(tracker_game, guess)
         assert tracker_game.state == state
         assert tracker_game.turns_left == turns_left
         tracker_game
